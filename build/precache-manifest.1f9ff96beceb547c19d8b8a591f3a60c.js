83798415ef877187d82a2699564c3a6ec91e1917
Versifiction <marc_charpentier@hotmail.fr>
2020-01-13 22:59:40 +0100

    edits texte

---
+++ build/precache-manifest.1f9ff96beceb547c19d8b8a591f3a60c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0d49580330354047d1a5b4aefc23554",
+    "url": "/static/media/photoshop.d0d49580.png"
+  },
+  {
+    "revision": "15257910ed3704a9e421",
+    "url": "/static/css/main.51df30b1.chunk.css"
+  },
+  {
+    "revision": "805c0bff588bda52713ba467e0dd3cbd",
+    "url": "/static/media/photo-head.805c0bff.jpg"
+  },
+  {
+    "revision": "07d22557d47f47543549",
+    "url": "/static/js/1.07d22557.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9def0c531dff4c6bf634915147b0f7a2",
+    "url": "/static/media/MC-Full-Transparent.9def0c53.png"
+  },
+  {
+    "revision": "7794051d2203285d59c640e66118fffc",
+    "url": "/static/media/cv.7794051d.pdf"
+  },
+  {
+    "revision": "0f437aa65279143c1063e1bd214f73c6",
+    "url": "/static/media/MC-Short-Transparent.0f437aa6.png"
+  },
+  {
+    "revision": "d54d6128321176f025a4015dc496bf53",
+    "url": "/static/media/elaboration.d54d6128.svg"
+  },
+  {
+    "revision": "98f6e854a7eceb49dde26b5962883832",
+    "url": "/static/media/realisation.98f6e854.svg"
+  },
+  {
+    "revision": "09f46c4964c864b8c3781b7a625664bc",
+    "url": "/static/media/conception.09f46c49.svg"
+  },
+  {
+    "revision": "e60e0b29bde24419e5ab9fca080c53b4",
+    "url": "/static/media/moi.e60e0b29.png"
+  },
+  {
+    "revision": "b23cf0127644d9ebfdd8c40fd5d29d0b",
+    "url": "/static/media/html.b23cf012.png"
+  },
+  {
+    "revision": "b13079d4477c4c476783c88a04202f51",
+    "url": "/static/media/jquery.b13079d4.png"
+  },
+  {
+    "revision": "21cef5f5ff8f0a831ed1692e91e7154a",
+    "url": "/static/media/react.21cef5f5.png"
+  },
+  {
+    "revision": "2e2cbb5e6bc6f1531149f12f0d43c31d",
+    "url": "/static/media/wordpress.2e2cbb5e.png"
+  },
+  {
+    "revision": "15257910ed3704a9e421",
+    "url": "/static/js/main.15257910.chunk.js"
+  },
+  {
+    "revision": "44de7b55c8813fe4aa6e3be53a1079ea",
+    "url": "/static/media/illustrator.44de7b55.png"
+  },
+  {
+    "revision": "05a9d66ec7235124088fb66066abf07e",
+    "url": "/static/media/apropos-head.05a9d66e.jpg"
+  },
+  {
+    "revision": "79eb1426ea8e300adadc2f475f7a6e38",
+    "url": "/static/media/memorymac.79eb1426.png"
+  },
+  {
+    "revision": "3df67309a4dfa0ac678fd6628b414cb4",
+    "url": "/static/media/orestomac.3df67309.png"
+  },
+  {
+    "revision": "9d5c49ceb914beff3b5a97c459348bed",
+    "url": "/static/media/calculatricemac.9d5c49ce.png"
+  },
+  {
+    "revision": "6bb50d9e57691b9b8c7d2e5fa91a915a",
+    "url": "/static/media/ratpmac.6bb50d9e.jpg"
+  },
+  {
+    "revision": "94f0f531fb9707427e0f99eb7414b9cc",
+    "url": "/static/media/chatmac.94f0f531.png"
+  },
+  {
+    "revision": "7ff389d348460fb5553701238dcbed21",
+    "url": "/static/media/plusmoinsmac.7ff389d3.png"
+  },
+  {
+    "revision": "7e5660c0396d447ee489029806c007da",
+    "url": "/static/media/mactodo.7e5660c0.jpg"
+  },
+  {
+    "revision": "b4444c7ead862eb3e55e4a3b0fe50bbe",
+    "url": "/static/media/newslettermac.b4444c7e.png"
+  },
+  {
+    "revision": "13f4568036c40ab9ba1771ff8f9e21ea",
+    "url": "/static/media/8marsmac.13f45680.png"
+  },
+  {
+    "revision": "16609b543ce8980a99b428a1213c8068",
+    "url": "/static/media/projets-head.16609b54.jpg"
+  },
+  {
+    "revision": "75b9e10a08e6e8f150d6c56331691bb6",
+    "url": "/static/media/contact-head.75b9e10a.jpg"
+  },
+  {
+    "revision": "57324af7453853f2ccce3c8008ffddb9",
+    "url": "/static/media/mentionslegales-head.57324af7.jpg"
+  },
+  {
+    "revision": "07d22557d47f47543549",
+    "url": "/static/css/1.15a6658c.chunk.css"
+  },
+  {
+    "revision": "cbbba490d2c2af6ab9fa3e00cab1ea95",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f313f808c6667277edc645ea5b3d8c47.js"
+  "/precache-manifest.1f9ff96beceb547c19d8b8a591f3a60c.js"
 );
 
 workbox.clientsClaim();
